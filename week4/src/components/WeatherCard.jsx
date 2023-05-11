21b820445bed3f6b6ec0de9160aaf0935b4d9ef4
HAE2NI <hae0_02ni@ewhain.net>
2023-05-11 12:14:36 +0900

    ✨ [Feat] Week 카드 보여주기

--- week4/src/components/WeatherCard.jsx
+++ week4/src/components/WeatherCard.jsx
@@ -1,61 +1,86 @@
-import { styled } from "styled-components";
-import axios from "axios";
 import React, { memo, useEffect, useState } from "react";
+import axios from "axios";
 import { useParams } from "react-router-dom";
 
-const WeatherCard = (props) => {
+import { St } from "./CardStyle";
+
+import { WEATER_TYPE } from "../constants/WeatherType";
+
+const WeatherCard = () => {
   const { area } = useParams();
-  const [data, setData] = useState({});
+  const [weather, setWeather] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const weatherInfo = () => {
     try {
+      setIsLoading(true);
       axios
         .get(
-          `https://api.openweathermap.org/data/2.5/weather?q=${area}&appid=${
+          `https://api.openweathermap.org/data/2.5/forecast?q=${area}&appid=${
             import.meta.env.VITE_APP_WEATHER
           }&units=metric`
         )
         .then((response) => {
-          console.log(response);
+          setWeather(response.data.list);
+          setIsLoading(false);
         });
     } catch (err) {
       console.log(err);
     }
   };
-  console.log(import.meta.env.VITE_APP_WEATHER);
+
+  const weatherList = weather.filter((_, index) => (index - 2) % 8 === 0);
+
   useEffect(() => {
     weatherInfo();
   }, [area]);
 
   return (
     <>
-      <St.CardWrapper>
-        <h2>안녕!</h2>
-        <h3>{name}</h3>
-        {/* <img src={}/> */}
-        <p></p>
-      </St.CardWrapper>
+      {isLoading ? (
+        <span>로딩중...</span>
+      ) : (
+        <>
+          {weatherList.map((data) => {
+            const weatherDescription =
+              data.weather?.length > 0 ? data.weather[0].description : "";
+            const imgSrc = WEATER_TYPE.find(
+              (data) => data.description === weatherDescription
+            );
+            return (
+              <St.CardWrapper key={data.dt}>
+                <St.CardHeader>
+                  <h3>{data?.dt_txt.slice(5, 10)}</h3>
+                </St.CardHeader>
+                <img src={imgSrc.imgURL} alt={imgSrc.description} />
+                <St.CardDesCription>
+                  <span>온도</span>
+                  <p>{data.main?.temp}</p>
+                </St.CardDesCription>
+
+                <St.CardDesCription>
+                  <span>체감 온도</span>
+                  <p>{data.main?.feels_like}</p>
+                </St.CardDesCription>
+
+                <St.CardDesCription>
+                  <span>최저/최고</span>
+                  <p>
+                    {data.main?.temp_min}/{data.main?.temp_max}
+                  </p>
+                </St.CardDesCription>
+
+                <St.CardDesCription>
+                  <span>구름</span>
+                  <p>{data.clouds?.all}%</p>
+                </St.CardDesCription>
+              </St.CardWrapper>
+            );
+          })}
+        </>
+      )}
     </>
   );
 };
 
 export default WeatherCard;
-
-const St = {
-  CardWrapper: styled.article`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-
-    gap: 1rem;
-    padding: 2rem;
-    border-radius: 1rem;
-
-    background-color: ${({ theme }) => theme.colors.coral};
-
-    h2 {
-      color: ${({ theme }) => theme.colors.white};
-    }
-  `,
-};
