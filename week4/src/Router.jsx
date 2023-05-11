6dcb4c15dca76bcbd468352dd8932253d4ce8d31
HAE2NI <hae0_02ni@ewhain.net>
2023-05-11 10:21:00 +0900

    ✨ [Feat] router 구현

--- week4/src/Router.jsx
+++ week4/src/Router.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Weather from "./pages/Weather";
 import DetailWeather from "./pages/DetailWeather";
+import WeatherCard from "./components/WeatherCard";
+import DayWeatherCard from "./components/DayWeatherCard";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Weather />} />
-        <Route path="/detail" element={<DetailWeather />} />
+        <Route path="/" element={<Weather />}>
+          <Route path="week/:area" element={<WeatherCard />} />
+          <Route path="day/:area" element={<DayWeatherCard />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
