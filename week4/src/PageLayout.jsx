dba3a71b11b4c43418cf53642eae74cc9d8c740e
HAE2NI <hae0_02ni@ewhain.net>
2023-05-11 10:23:04 +0900

    ✨ [Feat] pageLayout

---
+++ week4/src/PageLayout.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styled from "styled-components";
+import Header from "./components/Header";
+import SearchCity from "./components/SearchCity";
+
+const PageLayout = ({ children }) => {
+  return (
+    <St.PageWrapper>
+      <Header />
+      <SearchCity />
+      <St.PageWrapper>{children}</St.PageWrapper>
+    </St.PageWrapper>
+  );
+};
+
+export default PageLayout;
+
+const St = {
+  PageWrapper: styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    width: 100%;
+    height: 100%;
+
+    background-color: ${({ theme }) => theme.colors.light_yellow};
+  `,
+  PageSection: styled.section`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `,
+};
