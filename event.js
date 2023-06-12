b49907733b62a119b5833442cca5fefac51779de
HAE2NI <hae0_02ni@ewhain.net>
2023-06-12 16:16:13 +0900

    ♻️ [refactor]

--- event.js
+++ event.js
@@ -1,8 +1,11 @@
 import MyMenus from "./data.js";
-const hiddenClass = "hidden";
+const HIDDEN_CLASS = "hidden";
 
 // 데이터를 카테고리별로 나누기
 function filterCategory(filterdId) {
+  // return filterdId === "all"
+  //   ? MyMenus
+  //   : MyMenus.filter((menu) => menu.category === filterdId);
   switch (filterdId) {
     case "all":
       return MyMenus;
@@ -17,18 +20,17 @@ function filterCategory(filterdId) {
 
 // 버튼이 체크되어있는지 확인
 function isChecked(checkbox) {
-  const is_checked = checkbox.checked;
-  if (is_checked === true) {
+  const { checked, id } = checkbox;
+  if (checked) {
     showCategory(checkbox);
-    showCard(checkbox.id);
+    showCard(id);
   } else {
     discardCategory(checkbox);
-    hiddenCard(checkbox.id);
+    hiddenCard(id);
   }
 }
 const checkbox = document.querySelectorAll("input");
 checkbox.forEach((data) => {
-  console.log(data.id);
   data.addEventListener("change", function (event) {
     isChecked(event.target);
   });
@@ -44,16 +46,20 @@ const side = document.querySelector(".side");
 function showCategory(checkbox) {
   switch (checkbox.id) {
     case "all":
-      all.classList.remove(hiddenClass);
+      all.classList.remove(HIDDEN_CLASS);
+      hiddenCard("all");
       break;
     case "salad":
-      salad.classList.remove(hiddenClass);
+      salad.classList.remove(HIDDEN_CLASS);
+      hiddenCard("salad");
       break;
     case "sandwich":
-      sandwich.classList.remove(hiddenClass);
+      sandwich.classList.remove(HIDDEN_CLASS);
+      hiddenCard("sandwich");
       break;
     case "side":
-      side.classList.remove(hiddenClass);
+      side.classList.remove(HIDDEN_CLASS);
+      hiddenCard("side");
       break;
   }
 }
@@ -62,16 +68,20 @@ function showCategory(checkbox) {
 function discardCategory(checkbox) {
   switch (checkbox.id) {
     case "all":
-      all.classList.add(hiddenClass);
+      all.classList.add(HIDDEN_CLASS);
+      hiddenCard("all");
       break;
     case "salad":
-      salad.classList.add(hiddenClass);
+      salad.classList.add(HIDDEN_CLASS);
+      hiddenCard("salad");
       break;
     case "sandwich":
-      sandwich.classList.add(hiddenClass);
+      sandwich.classList.add(HIDDEN_CLASS);
+      hiddenCard("sandwich");
       break;
     case "side":
-      side.classList.add(hiddenClass);
+      side.classList.add(HIDDEN_CLASS);
+      hiddenCard("side");
       break;
   }
 }
@@ -81,8 +91,8 @@ function xbtnhiddenCard(filterdId) {
   const hiddenBtn = filterdId.parentElement;
   handleCheckbox(filterdId.className);
 
-  hiddenBtn.classList.add(hiddenClass);
-  hiddenCard(filterdId);
+  hiddenBtn.classList.add(HIDDEN_CLASS);
+  hiddenCard(filterdId.className);
 }
 
 const deleteBtn = document.querySelectorAll("button");
@@ -95,17 +105,24 @@ deleteBtn.forEach((data) => {
 //상단 태그에서 엑스버튼 눌렀을 때 체크박스의 체크 사라지기
 function handleCheckbox(checkId) {
   const checkbox = document.getElementById(`${checkId}`);
-  if (checkbox.checked === true) {
+  if (checkbox.checked) {
     checkbox.checked = false;
   }
 }
 
 //카드 만들기
 function makeCard(data) {
+  const newOne = JSON.parse(localStorage.getItem("new"));
+  if (newOne) {
+    MyMenus.push({
+      ...newOne,
+      category: "sandwich",
+      img: "./assets/sandwichsalmon.jpg",
+    });
+  }
+
   const cardArticle = document.createElement("article");
-  cardArticle.classList.add("card");
-  cardArticle.classList.add("slideUp");
-  cardArticle.classList.add(data.category);
+  cardArticle.classList.add("card", "slideUp", data.category);
 
   cardArticle.innerHTML = `
     <h3>${data.name}</h3>
@@ -141,7 +158,7 @@ function makeCard(data) {
 
   function modalOpener() {
     modalWrapper.appendChild(modal);
-    modalWrapper.classList.toggle(hiddenClass);
+    modalWrapper.classList.toggle(HIDDEN_CLASS);
   }
   hashtagMore.addEventListener("click", modalOpener);
 
@@ -165,7 +182,6 @@ function showCard(filterdId) {
 
   filteredData.map((data) => {
     let cards = makeCard(data);
-    // console.log(cards);
     cardSection.appendChild(cards);
   });
 }
@@ -183,58 +199,7 @@ function hiddenCard(filterdId) {
   });
 }
 
-//데이터 받아오기
-function newLoad() {
-  const cardSection = document.querySelector(".card_section");
-  let cards = newCard();
-  cardSection.appendChild(cards);
-}
-
-function newCard() {
-  let newOne = JSON.parse(localStorage.getItem("new"));
-  const cardArticle = document.createElement("article");
-  cardArticle.classList.add("card");
-  cardArticle.classList.add("slideUp");
-
-  cardArticle.innerHTML = `
-    <h3>${newOne.menu}</h3>
-    `;
-  const hashtagWrapper = document.createElement("div");
-  hashtagWrapper.className = "hashtag_wrapper";
-  const hashtagContainer = document.createElement("ul");
-  hashtagContainer.className = "hashtag";
-  cardArticle.appendChild(hashtagWrapper);
-  hashtagWrapper.appendChild(hashtagContainer);
-
-  newOne.tags.forEach((tag) => {
-    const hashtag = document.createElement("li");
-    hashtag.className = "hashtag_item";
-
-    hashtagContainer.appendChild(hashtag);
-    hashtag.innerText = tag;
-  });
-
-  const hashtagMore = document.createElement("button");
-  hashtagMore.innerText = "+";
-  hashtagMore.classList.add("hashtag_btn");
-  hashtagWrapper.appendChild(hashtagMore);
-
-  const menuImg = document.createElement("img");
-  menuImg.classList.add("stuff");
-  menuImg.src = "./assets/sandwichsalmon.jpg";
-  menuImg.alt = newOne.menu;
-  cardArticle.appendChild(menuImg);
-
-  const mineBtn = document.createElement("img");
-  mineBtn.src = "./assets/heartbtn.png";
-  mineBtn.alt = "찜버튼";
-  mineBtn.classList.add("minebtn");
-  cardArticle.appendChild(mineBtn);
-  return cardArticle;
-}
-
 //화면 새로고침하면 실행
 window.onload = function () {
   showCard("all");
-  newLoad();
 };
